0831d44bf234a9ab6a61f1d3181f7978942b4339
Sean McPherson <git@seanmcp.com>
2020-11-11 14:22:22 -0500

    feat: add config view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,107 +1,66 @@
+import { get } from "./search-params.js";
+
 (function main() {
   // Code
-  // n notes, t transition, r rotation, c color
-  // ?n=Reading%20groups%20>%20Independent%20reading%20>%20Online%20exercises%20>%20Word%20games&t=5&r=4&c=red&r=3&c=blue
-  const usp = new URLSearchParams(location.search);
-  const note = usp.get("n");
-  const transition = parseInt(usp.get("t"));
-  const rotation = usp.getAll("r");
-  const color = usp.getAll("c");
-  const rotations = rotation.map((r, i) => ({
-    length: parseInt(r),
-    color: color[i],
-  }));
+  const rotationsEl = document.getElementById("rotations");
+  const addRotationButton = document.getElementById("add-rotation");
 
-  let currentRotation = 0;
-  let currentTime = Number(rotations[currentRotation].length);
-  let state = "";
-  function setState(nextState) {
-    state = nextState;
-    document.body.dataset.state = state;
-    if (state === "rotation") {
-      document.body.dataset.color = rotations[currentRotation].color;
-    } else {
-      document.body.removeAttribute("data-color");
-    }
-  }
-  setState("rotation");
+  function addRotation({ length, color } = {}) {
+    const fieldEl = document.createElement("li");
+    fieldEl.classList.add("field", "rotation");
 
-  if (note) {
-    const noteEl = document.getElementById("note");
-    noteEl.removeAttribute("hidden");
-    noteEl.innerHTML += `<p>${note.replace(/\n/g, "<br>")}</p>`;
-  }
+    const lengthLabel = document.createElement("label");
+    const lengthB = document.createElement("b");
+    lengthB.textContent = "Length";
+    lengthLabel.appendChild(lengthB);
+    const lengthInput = document.createElement("input");
+    lengthInput.max = 60;
+    lengthInput.min = 1;
+    lengthInput.name = "r";
+    lengthInput.placeholder = "12";
+    lengthInput.type = "number";
+    if (length) lengthInput.value = length;
+    lengthLabel.appendChild(lengthInput);
+    fieldEl.appendChild(lengthLabel);
 
-  const rotationsUlEl = document.getElementById("rotations");
-  for (let i = 0; i < rotations.length; i++) {
-    rotationsUlEl.innerHTML += `<li data-rotation=${i}>Rotation ${i + 1}</li>`;
-    if (i < rotations.length - 1) {
-      rotationsUlEl.innerHTML += `<li data-transition=${i}>Transition</li>`;
-    }
-  }
-  function setCurrentRotationIndicator() {
-    rotationsUlEl.childNodes.forEach((node) =>
-      node.removeAttribute("aria-current")
-    );
-    rotationsUlEl
-      .querySelector(`[data-${state}="${currentRotation}"]`)
-      .setAttribute("aria-current", true);
-  }
-  setCurrentRotationIndicator()
+    const colorLabel = document.createElement("label");
+    const colorB = document.createElement("b");
+    colorB.textContent = "Color";
+    colorLabel.appendChild(colorB);
+    const colorSelect = document.createElement("select");
+    colorSelect.name = "c";
+    ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"].forEach((label) => {
+      const value = label[0].toLowerCase();
+      colorSelect.innerHTML += `<option value="${value}" ${
+        color && value === color ? "selected" : ""
+      }>${label}</option>`;
+    });
+    colorLabel.appendChild(colorSelect);
+    fieldEl.appendChild(colorLabel);
 
-  // TODO: Remove
-  console.log({ note, transition, rotations });
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("button", "subtle");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      fieldEl.remove();
+    });
+    fieldEl.appendChild(deleteButton);
 
-  const displayEl = document.getElementById("display");
-  const startButtonEl = document.getElementById("start");
-  const pauseButtonEl = document.getElementById("pause");
-
-  displayEl.textContent = currentTime;
-
-  function tick() {
-    currentTime--;
-    switch (state) {
-      case "rotation": {
-        if (currentTime < 0) {
-          if (currentRotation === rotations.length - 1) {
-            // TODO: End
-            return clearInterval(window._interval);
-            break;
-          }
-          // Move to transition
-          setState("transition");
-          currentTime = transition;
-          setCurrentRotationIndicator();
-        }
-        break;
-      }
-      case "transition": {
-        if (currentTime < 0) {
-          // Move to next rotation
-          currentRotation++;
-          currentTime = Number(rotations[currentRotation].length);
-          setState("rotation");
-          setCurrentRotationIndicator();
-        }
-        break;
-      }
-    }
-    displayEl.textContent = currentTime;
+    rotationsEl.appendChild(fieldEl);
   }
 
-  startButtonEl.addEventListener("click", (event) => {
+  addRotationButton.addEventListener("click", (event) => {
     event.preventDefault();
-    startButtonEl.hidden = true;
-    pauseButtonEl.removeAttribute("hidden");
-    // TODO: Start timer
-    window._interval = setInterval(tick, 1000);
+    addRotation();
   });
 
-  pauseButtonEl.addEventListener("click", (event) => {
-    event.preventDefault();
-    pauseButtonEl.hidden = true;
-    startButtonEl.removeAttribute("hidden");
-    // TODO: Pause timer
-    clearInterval(window._interval);
-  });
+  const params = get();
+  if (params.edit) {
+    document.querySelector('[name="n"]').value = params.note;
+    document.querySelector('[name="t"]').value = params.transition;
+    params.rotations.forEach(addRotation);
+  } else {
+    addRotation();
+  }
 })();
