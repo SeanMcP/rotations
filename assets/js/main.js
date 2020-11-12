42b8916b170b49cea90cda6a51d55d9a07633d28
Sean McPherson <git@seanmcp.com>
2020-11-12 15:58:59 -0500

    style: half decent

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,55 @@
+import el from "./el.js";
 import { get } from "./search-params.js";
 
 (function main() {
   // Code
-  const rotationsEl = document.getElementById("rotations");
+  const rotationsTable = document.getElementById("rotations-table");
   const addRotationButton = document.getElementById("add-rotation");
 
   function addRotation({ length, color } = {}) {
-    const li = document.createElement("li");
-    li.classList.add("rotation");
-    const fieldEl = document.createElement("div");
-    fieldEl.classList.add("field");
-    li.appendChild(fieldEl);
+    const row = document.createElement("tr");
+    row.classList.add("rotation");
 
-    const lengthLabel = document.createElement("label");
-    const lengthB = document.createElement("b");
-    lengthB.textContent = "Minutes";
-    lengthLabel.appendChild(lengthB);
-    const lengthInput = document.createElement("input");
-    lengthInput.max = 60;
-    lengthInput.min = 1;
-    lengthInput.name = "r";
-    lengthInput.placeholder = "12";
-    lengthInput.type = "number";
-    if (length) lengthInput.value = length;
-    lengthLabel.appendChild(lengthInput);
-    fieldEl.appendChild(lengthLabel);
+    const lengthCell = el("td");
+    const lengthInput = el("input", {
+      "aria-label": "Length",
+      max: 60,
+      min: 1,
+      name: "r",
+      placeholder: 12,
+      type: "number",
+      value: length ? length : null,
+    });
+    lengthCell.appendChild(lengthInput);
+    row.appendChild(lengthCell);
 
-    const colorLabel = document.createElement("label");
-    const colorB = document.createElement("b");
-    colorB.textContent = "Color";
-    colorLabel.appendChild(colorB);
-    const colorSelect = document.createElement("select");
-    colorSelect.name = "c";
+    const colorCell = el("td");
+    const colorSelect = el("select", {
+      "aria-label": "Color",
+      name: "c",
+    });
     ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"].forEach((label) => {
       const value = label[0].toLowerCase();
       colorSelect.innerHTML += `<option value="${value}" ${
         color && value === color ? "selected" : ""
       }>${label}</option>`;
     });
-    colorLabel.appendChild(colorSelect);
-    fieldEl.appendChild(colorLabel);
+    colorCell.appendChild(colorSelect);
+    row.appendChild(colorCell);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("button", "subtle");
-    deleteButton.textContent = "Delete";
+    const deleteCell = el("td");
+    const deleteButton = el("button", {
+      class: "button subtle",
+      textContent: "Delete",
+    });
     deleteButton.addEventListener("click", (event) => {
       event.preventDefault();
-      fieldEl.remove();
+      row.remove();
     });
-    fieldEl.appendChild(deleteButton);
+    deleteCell.appendChild(deleteButton);
+    row.appendChild(deleteCell);
 
-    rotationsEl.appendChild(li);
+    rotationsTable.appendChild(row);
   }
 
   addRotationButton.addEventListener("click", (event) => {
